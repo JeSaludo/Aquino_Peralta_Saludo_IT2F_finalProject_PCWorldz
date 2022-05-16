64273aeffa31c5647097c6ee6e1eefa462df35f5
AquinoShawnAaron <oniuqa773@gmail.com>
2022-05-16 12:56:51 +0800

    Customized Login Form

--- index.php
+++ index.php
@@ -36,18 +36,19 @@ if(isset($_REQUEST['login_button']) === true){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>iofrm</title>
+    <title>PC Worldz</title>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css">
     <link rel="stylesheet" type="text/css" href="css/iofrm-style.css">
     <link rel="stylesheet" type="text/css" href="css/iofrm-theme5.css">
+    <link rel="icon" href="favicon.ico">
 </head>
 <body>
     <div class="form-body">
         <div class="website-logo">
             <a href="index.php">
                 <div class="logo">
-                    <img class="logo-size" src="images/logo-light.svg" alt="">
+                    <img class="logo-size" src="images/logo-footer.png" alt="">
                 </div>
             </a>
         </div>
@@ -55,14 +56,14 @@ if(isset($_REQUEST['login_button']) === true){
             <div class="img-holder">
                 <div class="bg"></div>
                 <div class="info-holder">
-                    <img src="images/graphic2.svg" alt="">
+                    <img src="images/intro.png" alt="">
                 </div>
             </div>
             <div class="form-holder">
                 <div class="form-content">
                     <div class="form-items">
-                        <h3>Get more things done with Loggin platform.</h3>
-                        <p>Access to the most powerfull tool in the entire design and web industry.</p>
+                        <h3>Welcome to PC Worldz!</h3>
+                        <p>Buy and build your very own gaming PC right now with PC Worldz.</p>
                         <div class="page-links">
                             <a href="index.php" class="active">Login</a><a href="register.php">Register</a>
                         </div>
