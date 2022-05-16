0e61f31adde5ffdea99530703c13c05ff67963aa
SaludoJanEris <janerissaludo@gmail.com>
2022-05-16 10:26:29 +0800

    Added Login System using PHP

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php 
+
+SESSION_START();
+
+$acc_username = "admin@dev.com";
+$acc_password = "admin";
+
+$url_add = "https://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+if(isset($_REQUEST['login_button']) === true){
+	if($_REQUEST['form_username'] != $acc_username){
+			header("Location: ".$url_add."?notexist");
+	}
+	else if($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] != $acc_password){
+			header("Location: ".$url_add."?wrongpass");
+	}
+	else if($_REQUEST['form_username'] == $acc_username && $_REQUEST['form_password'] == $acc_password){
+			header("Location: ".$url_add."?success");
+
+			$_SESSION['ses_username'] = $acc_username;
+			$_SESSION['ses_password'] = $acc_password;
+			$_SESSION['ses_fullname'] = $acc_fullname;
+			$_SESSION['ses_address'] = $acc_address;
+			
+	}
+	
+}
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -31,13 +64,30 @@
                         <h3>Get more things done with Loggin platform.</h3>
                         <p>Access to the most powerfull tool in the entire design and web industry.</p>
                         <div class="page-links">
-                            <a href="login5.php" class="active">Login</a><a href="register5.php">Register</a>
+                            <a href="index.php" class="active">Login</a><a href="register.php">Register</a>
                         </div>
-                        <form>
-                            <input class="form-control" type="text" name="username" placeholder="E-mail Address" required>
-                            <input class="form-control" type="password" name="password" placeholder="Password" required>
+                        <form method="POST">
+                        <?php
+						if(isset($_REQUEST['notexist'])===true){
+							echo "<div class='alert alert-danger' role='alert'> Username doest not exist... </div>";
+						}
+						else if(isset($_REQUEST['wrongpass']) ===true){
+							echo "<div class='alert alert-warning' role='alert'> Incorrect Password... </div>";					
+						}
+						else if(isset($_REQUEST['success']) ===true){
+							echo "<div class='alert alert-success' role='alert'> Redirecting... </div>";	
+							header("Refresh: 5; url=home.php");			
+						}
+						else if(isset($_REQUEST['logout']) === true)
+						{
+							echo "<div class='alert alert-info' role='alert'> Thank you... </div>";	
+						}
+						?>
+                        
+                            <input class="form-control" type="text" name="form_username" placeholder="E-mail Address" required>
+                            <input class="form-control" type="password" name="form_password" placeholder="Password" required>
                             <div class="form-button">
-                                <button id="submit" type="submit" class="ibtn">Login</button> <a href="forget5.php">Forget password?</a>
+                                <button id="submit" type="submit" class="ibtn" name="login_button" >Login</button> <a href="forget.php">Forget password?</a>
                             </div>
                         </form>
                         <div class="other-links">
